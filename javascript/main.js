3e299ae6335c72f225c09d05129fa39cc521386d
Thorvald <91074943+Soralienne@users.noreply.github.com>
2022-03-09 15:40:30 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,69 @@
+var aText = new Array(
+  "| Thorabyrinthe", "Trouverez-vous le bon chemin ?" );
+  var iSpeed = 55;
+  var iIndex = 0;
+  var iArrLength = aText[0].length;
+  var iScrollAt = 20;
+  
+  var iTextPos = 0;
+  var sContents = '';
+  var iRow;
+  
+  function typewriter() {
+      sContents =  ' ';
+      iRow = Math.max(0, iIndex-iScrollAt);
+      var destination = document.getElementById("texttitre");
+  
+      while ( iRow < iIndex ) {
+          sContents += aText[iRow++] + '<br />';
+      }
+      destination.innerHTML = sContents + aText[iIndex].substring(0,
+  iTextPos) + " |";
+      if ( iTextPos++ == iArrLength ) {
+          iTextPos = 0;
+          iIndex++;
+          if ( iIndex != aText.length ) {
+              iArrLength = aText[iIndex].length;
+              setTimeout("typewriter()", 500);
+          }
+      }
+      else {
+          setTimeout("typewriter()", iSpeed);
+      }
+  }
+  typewriter();
+
+
+var h = window.innerHeight;
+var w = window.innerWidth;
+
+var platforms;
+var cursor;
+
+var player;
+var Connor;
+
+var doorGauche;
+var doorDroite;
+var doorStart;
+
+var fire1;
+var fire2;
+
+var wall1;
+var wall2;
+var wall3;
+var wall4;
+
+/** 
+var questString = [
+  ["type", "key", "next", "end"],
+  ["q", "Va parler à Connor", "", ""],
+  ["","", "Fini", "", ""],
+  ["q", "Fini", "", "1"],
+].map(x => x.join(",")).join("\n");
+*/
+
 class SceneStart extends Phaser.Scene {
 
   constructor() {
@@ -14,17 +80,74 @@ class SceneStart extends Phaser.Scene {
     this.load.image("playerLeftRun1", "javascript/assets/playerLeftRun1.png");
     this.load.image("playerLeftRun2", "javascript/assets/playerLeftRun2.png");
     this.load.image("door", "javascript/assets/doors.png");
-    this.load.image("wall", "javascript/assets/walls.png");
-    /** 
+    this.load.image("wall", "javascript/assets/walls.png"); 
     this.load.image("fireStart1", "javascript/assets/fireStart1.png");
     this.load.image("fireStart2", "javascript/assets/fireStart2.png");
     this.load.image("fireStart3", "javascript/assets/fireStart3.png");
-    */
+    
   }
+
+/**  
+  handleMeeting(player, Connor){
+    if(player._currentQuest){
+      console.info(player._currentQuest.currentQuest)
+      let options = player._currentQuest.currentQuest.options;
+      if (options[0].next == "Va parler à Connor") {
+        console.log("test mon reuf");
+        player._currentQuest.manager.getNextQuestion(options[0].next);
+      } else if (options[0].next == "Fini"){
+        this.doorStart.visible = true;
+        player._currentQuest.manager.getNextQuestion(options[0].next);
+        this.physics.add.collider(player, doorStart, collision, undefined, this);
+      }
+    }
+  }
+
+  checkDoorStatus(player, door){
+    return door.visible == false;
+}
+
+*/
 
   create() {
     cursor = this.input.keyboard.createCursorKeys(); //touches des fleches
     platforms = this.physics.add.staticGroup();
+
+    player = this.physics.add.sprite((w / 2), h, "player"); //joueur
+    player.setScale(0.2);
+    player.setCollideWorldBounds(true); //collision avec la bordure
+
+/** 
+
+    Connor = this.physics.add.sprite(160, 300, "player")
+    Connor.setScale(1, 1);
+    Connor.body.setSize(30, 35);
+
+    this.print = this.add.text(380, 180, '', { fontSize: '12px', align: 'right' }).setOrigin(1);
+
+    this.plugins.get('rexquestplugin').add({
+      questions: questString,
+      quest: true
+    }).on('quest', function (currentQuest, manager, quest) {
+      // La quête est fini
+      if (currentQuest.end === 1) {
+          manager.setData('endAt', currentQuest.key);
+          manager.emit('complete', manager, quest);
+      } else {
+          // Prochaine étape de la quête
+          if(player._currentQuest ){ 
+              this.print.text = this.print.text + 'done\n';
+          }
+          this.print.text += `${currentQuest.key}...`;
+          player._currentQuest = { currentQuest, manager}
+      }
+  }, this).on('complete', function (manager, quest) {
+      delete player._currentQuest;
+      this.print.text = this.print.text + 'done\n';
+      this.print.text += `\nLa porte est debloquée!`;
+  }, this).getNextQuestion();
+
+*/
 
     //Les animations 
     this.anims.create({
@@ -45,22 +168,25 @@ class SceneStart extends Phaser.Scene {
       repeat : 0
     })
 
-    /**
-     * this.anims.create({
-     * key : "fireMouvement",
-     * frames : [
-     * {key : "fireStart1"},
-     * {key : "fireStart2"},
-     * {key : "fireStart3"}],
-     * framerate : 7,
-     * repeat : -1
-     * })
-     */
-
-    player = this.physics.add.sprite((w / 2), h, "player"); //joueur
-    player.setScale(1, 1);
-    player.body.setSize(30, 35);
     
+    this.anims.create({
+    key : "fireMouvement",
+      frames : [
+      {key : "fireStart2"},
+      {key : "fireStart1"},
+      {key : "fireStart3"}],
+      frameRate : 6,
+      repeat : -1
+    })
+     
+    fire1 = this.add.sprite(350, 60, "fireStart1")
+    fire1.setScale(0.5);
+    fire1.anims.play("fireMouvement", true)
+
+    fire2 = this.add.sprite(1050, 60, "fireStart1")
+    fire2.setScale(0.5);
+    fire2.anims.play("fireMouvement", true)
+
     wall1 = this.add.sprite(200, 146, "wall");
     wall1.setScale(0.3);
 
@@ -85,48 +211,48 @@ class SceneStart extends Phaser.Scene {
     platforms.add(wall2);
     platforms.add(wall3);
     platforms.add(wall4);
+
     this.physics.add.collider(platforms, player); //collision
-    player.setCollideWorldBounds(true); //collision avec la bordure
+    this.physics.add.collider(player, this.Connor, this.handleMeeting, undefined, this);
+    
     
     //Fonction de collision qui éxecute le code dedans quand la fonctions est appelé
     function collision() {
       this.scene.start("labyrintheStart");
     }
-    
-    this.physics.add.collider(player, doorStart, collision, undefined, this);
   }
 
   update() {
 
     // Tous les mouvement sont controler par ce code
   
-      if (cursor.left.isDown){
-        player.setVelocityX(-200); //vitesse de deplacements
-        player.anims.play("playerWalkLeft", true); //animations du personnage
-        player.setFlip(false, false); //oriantation de l'image
-      } else if (cursor.right.isDown){
-        player.setVelocityX(200);
-        player.anims.play("playerWalkLeft", true);
-        player.setFlip(true, false);
-      } else if (cursor.up.isDown){
-        player.setVelocityY(-200);
-        player.anims.play("playerWalkUp", true);
-        player.setFlip(false, false);
-      } else if (cursor.down.isDown){
-        player.setVelocityY(200);
-        player.anims.play("playerWalkUp", true);
-        player.setFlip(false, true);
-      } else {
-        player.setVelocity(0);
-        player.setTexture("player");
-      }
-  
-      if ((cursor.left.isDown && cursor.up.isDown) || (cursor.left.isDown && cursor.right.isDown) || (cursor.left.isDown && cursor.down.isDown) || (cursor.right.isDown && cursor.up.isDown) || (cursor.right.isDown && cursor.down.isDown)){
-        player.setVelocity(0);
-        player.setTexture("player");
-      }
-  
-    //--------
+    if (cursor.left.isDown){
+      player.setVelocityX(-200); //vitesse de deplacements
+      player.anims.play("playerWalkLeft", true); //animations du personnage
+      player.setFlip(false, false); //oriantation de l'image
+    } else if (cursor.right.isDown){
+      player.setVelocityX(200);
+      player.anims.play("playerWalkLeft", true);
+      player.setFlip(true, false);
+    } else if (cursor.up.isDown){
+      player.setVelocityY(-200);
+      player.anims.play("playerWalkUp", true);
+      player.setFlip(false, false);
+    } else if (cursor.down.isDown){
+      player.setVelocityY(200);
+      player.anims.play("playerWalkUp", true);
+      player.setFlip(false, true);
+    } else {
+      player.setVelocity(0);
+      player.setTexture("player");
+    }
+
+    if ((cursor.left.isDown && cursor.up.isDown) || (cursor.left.isDown && cursor.right.isDown) || (cursor.left.isDown && cursor.down.isDown) || (cursor.right.isDown && cursor.up.isDown) || (cursor.right.isDown && cursor.down.isDown)){
+      player.setVelocity(0);
+      player.setTexture("player");
+    }
+
+  //--------
   }
 }
 
@@ -146,7 +272,7 @@ class LabyrintheStart extends Phaser.Scene {
     this.load.image("wall", "javascript/assets/walls.png"); 
     this.load.image("door", "javascript/assets/doors.png");
   }
-
+   
   create() {
     cursor = this.input.keyboard.createCursorKeys();
     platforms = this.physics.add.staticGroup();
@@ -221,146 +347,19 @@ class LabyrintheStart extends Phaser.Scene {
       }
   }
 }
-/**
-class LabyrintheDeux extends Phaser.Scene {
-
-  constructor() {
-    super({key: "labyrintheDeux"});
-  }
-
-  preload() {
-    this.load.plugin('rexquestplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexquestplugin.min.js', true);
-
-    this.load.image("player", "javascript/assets/player.png");
-    this.load.image("run1", "javascript/assets/run1.png");
-    this.load.image("run2", "javascript/assets/run2.png");
-    this.load.image("playerLeftRun1", "javascript/assets/playerLeftRun1.png");
-    this.load.image("playerLeftRun2", "javascript/assets/playerLeftRun2.png");
-    this.load.image("wall", "javascript/assets/wall.png"); 
-    this.load.image("doorStart", "javascript/assets/door.png");
-  }
-
-  create() {
-    cursor = this.input.keyboard.createCursorKeys();
-    platforms = this.physics.add.staticGroup();
-
-    this.anims.create({
-      key : "playerWalkUp",
-      frames : [
-        {key : "run1"},
-        {key : "run2"}],
-      frameRate : 7,
-      repeat : 0
-    })
-
-    this.anims.create({
-      key : "playerWalkLeft",
-      frames : [
-        {key : "playerLeftRun1"},
-        {key : "playerLeftRun2"}],
-      frameRate : 7,
-      repeat : 0
-    })
-
-    player = this.physics.add.sprite((w - w) + 70, h, "player");
-    player.setScale(1, 1);
-    player.body.setSize(30, 35);
-    
-    wall = this.add.sprite((w - w) + 6, h - 126, "wall");
-
-    door = this.physics.add.staticSprite((w / 2) - 20, 30, "door");
-    door.rotation += 20.42;
-    door.setSize(50, 10);
-
-    platforms.add(wall);
-    this.physics.add.collider(platforms, player);
-    player.setCollideWorldBounds(true);
-    
-    function collision() {
-      this.scene.start("");
-    }
-    
-    this.physics.add.collider(player, door, collision, undefined, this);
-  }
-
-  update() {
-  
-      if (cursor.left.isDown){
-        player.setVelocityX(-200);
-        player.anims.play("playerWalkLeft", true);
-        player.setFlip(false, false);
-      } else if (cursor.right.isDown){
-        player.setVelocityX(200);
-        player.anims.play("playerWalkLeft", true);
-        player.setFlip(true, false);
-      } else if (cursor.up.isDown){
-        player.setVelocityY(-200);
-        player.anims.play("playerWalkUp", true);
-        player.setFlip(false, false);
-      } else if (cursor.down.isDown){
-        player.setVelocityY(200);
-        player.anims.play("playerWalkUp", true);
-        player.setFlip(false, true);
-      } else {
-        player.setVelocity(0);
-        player.setTexture("player");
-      }
-  
-      if ((cursor.left.isDown && cursor.up.isDown) || (cursor.left.isDown && cursor.right.isDown) || (cursor.left.isDown && cursor.down.isDown) || (cursor.right.isDown && cursor.up.isDown) || (cursor.right.isDown && cursor.down.isDown)){
-        player.setVelocity(0);
-        player.setTexture("player");
-      }
-  }
-
-}
-*/
 
 var config = {
   type: Phaser.AUTO,
-  width: window.innerWidth - 20,
-  height: window.innerHeight - 100,
+  width: 1346,
+  height: 561,
   backgroundColor: "#FFFFFF", //#FFFFFF
   physics: {
     default : "arcade",
     arcade : {
-      debug : false,
+      debug : true,
     }
   },
-  scene: [SceneStart, LabyrintheStart /**, LabyrintheDeux*/]
+  scene: [SceneStart, LabyrintheStart]
 };
 
 let game = new Phaser.Game(config);
-
-var h = window.innerHeight;
-var w = window.innerWidth;
-
-var platforms;
-var cursor;
-var player;
-
-var doorGauche;
-var doorDroite;
-var doorUp;
-var doorStart;
-
-var wall1;
-var wall2;
-var wall3;
-var wall4;
-
-
-// A rajouter plus tard
-/**
- * 
-    function collisionUp() {
-      this.scene.start("labyrintheTrois");
-    }
- * 
- * this.physics.add.collider(player, doorGauche, collisionUp, undefined, this); 
- * 
- * doorUp = this.physics.add.staticSprite((w / 2) - 20, 8, "door");
- * doorUp.setScale(0.08);
- * doorUp.setSize(80, 18);
- * doorUp.setOffset(55, 491);
- * doorUp.rotation += -20.42;
-*/
